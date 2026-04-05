82c1b397ea9a582be7eaa3076faf56f0f2234ea4
rahulrupanibabal <rahulrupanibabal@gmail.com>
2026-04-05 18:47:20 +0000

    Add basic strcmp utility for shell commands

--- string.c
+++ string.c
@@ -8,3 +8,10 @@ void* memset(void* ptr, int value, int num) {
     }
     return ptr;
 }
+int strcmp(const char* s1, const char* s2) {
+    while (*s1 && (*s1 == *s2)) {
+        s1++;
+        s2++;
+    }
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
