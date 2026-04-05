f2addd43f6f65644e867da8a51c749885825144c
rahulrupanibabal <rahulrupanibabal@gmail.com>
2026-04-05 18:45:42 +0000

    Implement memset for memory initialization

--- string.c
+++ string.c
@@ -1,2 +1,10 @@
 #include "string.h"
 #include "types.h"
+
+void* memset(void* ptr, int value, int num) {
+    unsigned char* p = ptr;
+    while (num--) {
+        *p++ = (unsigned char)value;
+    }
+    return ptr;
+}
