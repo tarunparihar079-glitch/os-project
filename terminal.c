7da1a0dada112965e11c3724889db1f2bf49e88b
241145-Team <241145@iiitt.ac.in>
2026-04-05 09:43:25 +0000

    Implement dynamic terminal text color setting

--- terminal.c
+++ terminal.c
@@ -4,3 +4,7 @@ static int terminal_row;
 static int terminal_column;
 terminal_row = 0;
 terminal_column = 0;
+uint8_t terminal_color;
+void terminal_setcolor(uint8_t color) {
+    terminal_color = color;
+}
