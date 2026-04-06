766287fcd540ad82f61b8785da4446b1da960909
Tarun Parihar <tarunparihar079@gmail.com>
2026-04-06 13:17:36 +0000

    Call IDT initialization on kernel boot

--- kernel.c
+++ kernel.c
@@ -1,8 +1,10 @@
+#include "idt.h"
 #include "terminal.h"
 #include "gdt.h"
 
 void kernel_main(void) {
     init_gdt();
+    init_idt();
     terminal_initialize();
     terminal_writestring("FirstOS/Users> ");
 }
