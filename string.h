286b69e1a9f15f51c1cea44f26c05ce96bd1aa5f
rahulrupanibabal <rahulrupanibabal@gmail.com>
2026-04-06 13:37:20 +0000

    Export memcpy utility

--- string.h
+++ string.h
@@ -4,5 +4,5 @@
 void* memset(void* ptr, int value, int num);
 int strcmp(const char* s1, const char* s2);
 char* strcpy(char* dest, const char* src);
-
+void* memcpy(void* dest, const void* src, int num);
 #endif
