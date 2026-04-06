932600484e89d47d29c32f3e8f179c90a4165e02
rahulrupanibabal <rahulrupanibabal@gmail.com>
2026-04-06 13:39:27 +0000

    Export itoa function

--- string.h
+++ string.h
@@ -4,5 +4,6 @@
 void* memset(void* ptr, int value, int num);
 int strcmp(const char* s1, const char* s2);
 char* strcpy(char* dest, const char* src);
+void itoa(int n, char* str);
 void* memcpy(void* dest, const void* src, int num);
 #endif
