3afd7d2c140e9c9338aa0345853a04ad3e5bf064
241145-Team <241145@iiitt.ac.in>
2026-04-06 13:25:43 +0000

    Add hardware cursor update function

--- terminal.c
+++ terminal.c
@@ -1,3 +1,4 @@
+#include "io.h"
 #include "terminal.h"
 // Terminal functions will go here
 static int terminal_row;
@@ -7,4 +8,11 @@ terminal_column = 0;
 uint8_t terminal_color;
 void terminal_setcolor(uint8_t color) {
     terminal_color = color;
+}
+void update_cursor(int x, int y) {
+    uint16_t pos = y * 80 + x;
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t) (pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
 }
