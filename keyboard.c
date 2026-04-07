a3da1fa7082906f0c747fc60c2778f3c590db5ce
241145-Team <241145@iiitt.ac.in>
2026-04-07 13:37:30 +0000

    Implement basic US keyboard scancode mapping

--- keyboard.c
+++ keyboard.c
@@ -1,5 +1,9 @@
 #include "keyboard.h"
 #include "io.h"
+const char kbd_us[128] = {
+    0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n'
+};
 void init_keyboard() {
     // Enable keyboard IRQ
 }
