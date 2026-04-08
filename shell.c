5b50ba289b6fe022e5ef856b88529d8c7efd3e2b
Tarun Parihar <tarunparihar079@gmail.com>
2026-04-08 16:19:02 +0000

    Implement shell command executor (help, clear)

--- shell.c
+++ shell.c
@@ -1,3 +1,4 @@
+#include "string.h"
 #include "shell.h"
 #include "terminal.h"
 char cmd_buffer[256];
@@ -6,4 +7,16 @@ void shell_input(char c) {
     if (cmd_idx < 255 && c != '\b') {
         cmd_buffer[cmd_idx++] = c;
     }
+}
+void execute_command() {
+    cmd_buffer[cmd_idx] = '\0'; // End string
+    if (strcmp(cmd_buffer, "help") == 0) {
+        terminal_writestring("\nCommands: help, clear");
+    } else if (strcmp(cmd_buffer, "clear") == 0) {
+        // Clear logic will trigger here
+    } else if (cmd_idx > 0) {
+        terminal_writestring("\nUnknown command");
+    }
+    cmd_idx = 0; // Reset buffer
+    terminal_writestring("\nFirstOS/Users> ");
 }
